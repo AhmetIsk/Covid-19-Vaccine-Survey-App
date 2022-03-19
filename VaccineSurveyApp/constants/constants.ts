50c821a4b807fe205ddb7b4ceeb14db65ca0f61e
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-03-19 22:18:04 +0300

    finish welcome page style and functionality

---
+++ VaccineSurveyApp/constants/constants.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  disabledText: "#9ca3af",
+  gray: "rgb(150, 150, 150)",
+  headerRed: "rgb(198, 21, 21)",
+  white: "#FFFFFF",
+  black: "#000",
+  green: "#41A426",
+  softGreen: "#70e45a",
+  blue: "#1C5AC7",
+  purple: "#A020F0",
+  orange: "#f25b05",
+  fadeOrange: "#f9ae83",
+  subtitle: "#4b5563",
+  title: "#1f2937",
+  termscond: "#242323",
+  borderColor: "#bec5d1",
+  disabled: "#f4f4f4",
+};
--- VaccineSurveyApp/screens/WelcomeScreen/WelcomeScreen.tsx
+++ VaccineSurveyApp/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,16 +1,31 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { colors } from "../../constants/constants";
 
 export default function WelcomeScreen({ navigation }: { navigation: any }) {
   return (
     <View style={styles.container}>
-      <Text>Covid 19 Vaccine Survey</Text>
+      <Image
+        source={require("../../assets/vaccineLogo.png")}
+        style={{ width: 300, height: 236 }}
+      />
+      <Text style={styles.header}>Covid 19 Vaccine Survey</Text>
+      <Text style={styles.description}>
+        Vaccine Pollster is requesting input from employees regarding their
+        COVID-19 vaccination status and how Vaccine Pollster may help to
+        facilitate vaccinations for employees. This anonymous and voluntary
+        survey will help senior management make decisions regarding reopening
+        the office; however, the results of this survey will not be the only
+        information used in the decision-making process. At this time, Vaccine
+        Pollster has no intention of mandating the COVID-19 vaccine.
+      </Text>
       <TouchableOpacity
         onPress={() => {
           navigation.navigate("Survey Page");
         }}
+        style={styles.startButton}
       >
-        <Text>Take the Survey Now!</Text>
+        <Text style={styles.buttonLabel}>Take the Survey Now!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -19,8 +34,28 @@ export default function WelcomeScreen({ navigation }: { navigation: any }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: `${colors.white}`,
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    margin: 20,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: `${colors.orange}`,
+  },
+  startButton: {
+    padding: 20,
+    backgroundColor: `${colors.orange}`,
+    borderRadius: 12,
+  },
+  buttonLabel: {
+    fontSize: 20,
+    color: `${colors.white}`,
+  },
+  description: {
+    padding: 20,
+    color: `${colors.disabledText}`,
+    fontStyle: "italic",
   },
 });
