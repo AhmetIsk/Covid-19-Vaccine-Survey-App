80b6344179c1cf84a8e2860e428e2865475415da
Ahmet Işık <a.isik@ug.bilkent.edu.tr>
2022-03-21 13:49:43 +0300

    finish thank you screen

--- VaccineSurveyApp/screens/ThankYouScreen/ThankYouScreen.tsx
+++ VaccineSurveyApp/screens/ThankYouScreen/ThankYouScreen.tsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { colors } from "../../constants/constants";
 
-const ThankYouScreen = () => {
+const ThankYouScreen = ({ navigation }: { navigation: any }) => {
   return (
     <View style={styles.container}>
-      <Text>ThankYouScreen</Text>
+      <Image
+        source={require("../../assets/thankYou.png")}
+        style={{ width: 250, height: 250 }}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Welcome")}
+        style={styles.backButton}
+      >
+        <Text style={styles.buttonLabel}>Back to Home Page</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,8 +24,19 @@ export default ThankYouScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  backButton: {
+    marginTop: 120,
+    padding: 10,
+    backgroundColor: `${colors.red}`,
+    borderRadius: 12,
+  },
+  buttonLabel: {
+    fontSize: 15,
+    color: `${colors.white}`,
   },
 });
